65c7246da70039b91c48493b33b17c8309bc6edb
akelii <elifak.35@gmail.com>
2016-10-17 02:35:37 +0300

    port number change for bluemix

--- server.py
+++ server.py
@@ -7,24 +7,25 @@ import os
 
 
 
-def create_app():
-    app = Flask(__name__)
+#def create_app():
+#    app = Flask(__name__)
 #    app.config.from_object('settings')
-    app.register_blueprint(site)
-    return app
+#    app.register_blueprint(site)
+#    return app
 
 
 
-def main():
-    app = create_app()
-    app.run()
+#def main():
+#    app = create_app()
+#    app.run()
 
 
 if __name__ == '__main__':
-    main()
-#    VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
-#    if VCAP_APP_PORT is not None:
-#        port, debug = int(VCAP_APP_PORT), False
-#    else:
-#        port, debug = 5000, True
-#    app = create_app()
+    VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
+    if VCAP_APP_PORT is not None:
+        port, debug = int(VCAP_APP_PORT), False
+    else:
+        port, debug = 5000, True
+    app = Flask(__name__)
+    app.register_blueprint(site)
+    app.run(host='0.0.0.0', port=port, debug=debug)
