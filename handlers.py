d4edd81afd412d77696ff057298a51585740c014
Mehmet Barış Yaman <oneymousbaris@gmail.com>
2016-10-16 23:48:16 +0300

    Following People-Projects pages are fixed

--- handlers.py
+++ handlers.py
@@ -18,3 +18,15 @@ def projects_create_page():
 @site.route('/project_search')
 def projects_search_page():
     return render_template('projects/search_project.html')
+
+@site.route('/register')
+def register_page():
+    return render_template('register.html')
+
+@site.route('/people_connections')
+def connections_following_people():
+    return render_template('connections/following_people.html')
+
+@site.route('/project_connections')
+def connections_following_projects():
+    return render_template('connections/following_projects.html')
