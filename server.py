0711e5a54b6477714177b2a7a7fa0d7d9f67b286
seferciogluecce <ecenazsefer@hotmail.com>
2016-10-27 23:14:51 +0300

    configuration on server.py to add link to database

--- server.py
+++ server.py
@@ -5,8 +5,6 @@ from handlers import  site
 import datetime
 import os
 
-
-
 #def create_app():
 #    app = Flask(__name__)
 #    app.config.from_object('settings')
@@ -20,12 +18,33 @@ import os
 #    app.run()
 
 
+
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
+
+
+
 if __name__ == '__main__':
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
+        
+        VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='dxxbzlpn' password='b_e_BTFVmUQvEpr-arXGfL25XHdaVrCX'
+                               host='jumbo.db.elephantsql.com' port=5432 dbname='dxxbzlpn'"""
+        
     app = Flask(__name__)
     app.register_blueprint(site)
     app.run(host='0.0.0.0', port=port, debug=debug)
