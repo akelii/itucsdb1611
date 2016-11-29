40ecad18a441316387b2f653bcacbfd0ecf4913c
Gülçin Baykal <gulcinbaykal@hotmail.com>
2016-11-29 13:47:05 +0300

    deleted unused post method

--- templates_operations/projects/project_details.py
+++ templates_operations/projects/project_details.py
@@ -18,21 +18,7 @@ def project_details_page_config(submit_type, key):
         project = store.get_project(key)
         project_comments = store_comment.get_project_comments(key)
         return render_template('projects/project_details.html', project=project, project_comments=project_comments)
-    else:
-       # if request and 'update' in request.form and request.method == 'POST':
-       #     project = store.get_project(key)
-       #     return redirect(url_for('site.projects_update_page'), key=key)
-            #return render_template('projects/project_update.html',  project=project)
-            #return render_template('dashboard.html')
-        if request and 'add' in request.form and request.method == 'POST':
-            person_id = 2
-            project_id = key
-            comment = request.form['project_comment']
-            create_date = datetime.datetime.now()
-            update_date = None
-            project_comment = ProjectComment(None, person_id, project_id, comment, create_date, update_date)
-            store_comment.add_project_comment(project_comment)
-            return redirect(url_for('site.projects_detail_page', key=key))
+
 
 
 
