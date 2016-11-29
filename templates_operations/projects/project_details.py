000daa43e2943764927027bddc29f2bb45eeabdb
Gülçin Baykal <gulcinbaykal@hotmail.com>
2016-11-29 16:57:36 +0300

    added new conditions for posting

--- templates_operations/projects/project_details.py
+++ templates_operations/projects/project_details.py
@@ -13,13 +13,37 @@ import psycopg2 as dbapi2
 
 def project_details_page_config(submit_type, key):
     store = project_operations()
-    store_comment = project_comment_operations()
+    store_comments = project_comment_operations()
     if submit_type == 'GET':
         project = store.get_project(key)
-        project_comments = store_comment.get_project_comments(key)
+        project_comments = store_comments.get_project_comments(key)
         return render_template('projects/project_details.html', project=project, project_comments=project_comments)
-   
-
+    else:
+        if 'addComment' in request.form:
+            person_id = 2
+            commented_project_id = int(key)
+            comment = request.form['project_comment']
+            project_comment = ProjectComment(None, person_id, commented_project_id, comment)
+            store_comments.add_project_comment(project_comment)
+            return redirect(url_for('site.projects_details_page', key=key))
+        elif 'update' in request.form:
+            title = request.form['project_name']
+            project_description = request.form['project_description']
+            end_date = None
+            member_limit = request.form['limit']
+            manager = 2
+            deleted = '0'
+            store.update_project(int(key), title, project_description, end_date, member_limit, manager, deleted)
+            return redirect(url_for('site.projects_details_page', key=key))
+        elif 'delete' in request.form:
+            comment_key = request.form['delete']
+            store_comments.delete_project_comment(int(comment_key))
+            return redirect(url_for('site.projects_details_page', key=key))
+        elif 'edit' in request.form:
+            comment_key = request.form['edit']
+            new_comment = request.form['newComment']
+            store_comments.update_project_comment(comment_key, new_comment, False)
+            return redirect(url_for('site.projects_details_page', key=key))
 
 
 
