5571af75dcfde95a78cc8a2ec2b2cd4eb056b2a2
gullukatik <katik@itu.edu.tr>
2016-12-05 04:11:28 +0300

    get method to list worklogs on project details page

--- templates_operations/projects/project_details.py
+++ templates_operations/projects/project_details.py
@@ -8,16 +8,20 @@ from classes.project_comment import ProjectComment
 from classes.operations.project_operations import project_operations
 from classes.operations.project_comment_operations import project_comment_operations
 from classes.project import Project
+from classes.operations.work_log_operations import work_log_operations
 from classes.model_config import dsn
 import psycopg2 as dbapi2
 
 def project_details_page_config(submit_type, key):
     store = project_operations()
     store_comments = project_comment_operations()
+    store_worklogs = work_log_operations()
     if submit_type == 'GET':
         project = store.get_project(key)
         project_comments = store_comments.get_project_comments(key)
-        return render_template('projects/project_details.html', project=project, project_comments=project_comments)
+        worklogs = store_worklogs.GetWorkLogByProjectId(key)
+        return render_template('projects/project_details.html', project=project, project_comments=project_comments,
+                               worklogs=worklogs)
     else:
         if 'addComment' in request.form:
             person_id = 2
