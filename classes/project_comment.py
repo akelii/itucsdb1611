e28e8c976c57fd8d370b33987fd2eee16ef4ac46
Gülçin Baykal <gulcinbaykal@hotmail.com>
2016-12-13 21:29:57 +0300

    CreateDate and UpdateDate attributes are added

--- classes/project_comment.py
+++ classes/project_comment.py
@@ -1,7 +1,9 @@
 class ProjectComment:
-    def __init__(self, ObjectId, PersonId, CommentedProjectId, Comment):
+    def __init__(self, ObjectId, PersonId, CommentedProjectId, Comment, CreateDate, UpdateDate):
         self.ObjectId = ObjectId
         self.PersonId = PersonId
         self.CommentedProjectId = CommentedProjectId
         self.Comment = Comment
+        self.CreateDate = CreateDate
+        self.UpdateDate = UpdateDate
         self.Deleted = 0
