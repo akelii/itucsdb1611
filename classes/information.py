1d5b954602da3e0df7ced78c002f7e4dfff5f36d
Gülçin Baykal <gulcinbaykal@hotmail.com>
2016-12-13 21:24:34 +0300

    Information class is created

---
+++ classes/information.py
@@ -0,0 +1,7 @@
+class Information:
+    def __init__(self, objectid, cvid, information_type_id, description):
+        self.objectid = objectid
+        self.cvid = cvid
+        self.information_type_id = information_type_id
+        self.description = description
+        self.deleted = 0
