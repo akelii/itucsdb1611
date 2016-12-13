67223b4c140d10f0201042a7e6b84b73d6b2069b
Gülçin Baykal <gulcinbaykal@hotmail.com>
2016-12-13 21:35:14 +0300

    current_user is assigned as creator

--- templates_operations/projects/create_project.py
+++ templates_operations/projects/create_project.py
@@ -2,19 +2,23 @@ from flask import render_template
 from flask import url_for
 from flask import redirect
 from flask import request
-from datetime import datetime
+from flask_login import current_user, login_required
 from datetime import datetime
 from classes.operations.project_operations import project_operations
+from classes.operations.person_operations import person_operations
 from classes.project import Project
 from classes.look_up_tables import *
+from templates_operations.user import*
+
 
 def project_create_page_config(submit_type):
-    if submit_type=='GET':
+    if submit_type == 'GET':
         listProjectType = GetProjectType()
         listProjectThesisType = GetProjectThesisType()
         listDepartment = GetDepartment()
         listProjectStatusType = GetProjectStatusType()
-        return render_template('projects/create_project.html', listProjectType=listProjectType, listProjectThesisType=listProjectThesisType, listDepartment=listDepartment, listProjectStatusType=listProjectStatusType)
+        listManager = GetManagerList()
+        return render_template('projects/create_project.html', listProjectType=listProjectType, listProjectThesisType=listProjectThesisType, listDepartment=listDepartment, listProjectStatusType=listProjectStatusType, listManager=listManager)
     else:
         if 'Add' in request.form.values():
             store = project_operations()
@@ -23,17 +27,16 @@ def project_create_page_config(submit_type):
             project_type = request.form['project_type']
             project_thesis_type = request.form['project_thesis_type']
             department = request.form['department']
-            project_status_type = 1
-        #    project_type = 1
-        #    project_thesis_type = None
-        #    department = 1
-        #    project_status_type = 1
-            start_date = datetime.datetime.now()
-            end_date = None
-            member_limit = 4
-            created_by = 2
-            manager = 2
+            start_date = datetime.datetime.now()#datepicker gelince değişecek
+            end_date = datetime.datetime.now()#datepicker gelince değişecek
+            if end_date > datetime.datetime.now():#ileri tarihte bitecekse
+                project_status_type = 2
+            else:#süre bitmişse
+                project_status_type = 3
+            member_limit = request.form['limit']
+            created_by = person_operations.GetPerson(current_user, current_user.email)[0]#current_user proje oluşturuyor
+            manager = request.form['project_manager']
             project = Project(None, title, project_description, project_type, project_thesis_type, department,
                               project_status_type, start_date, end_date, member_limit, None, created_by, manager)
             store.add_project(project)
-            return render_template('dashboard.html')
+            return redirect(url_for('site.home_page'))
