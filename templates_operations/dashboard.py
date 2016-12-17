638aec926851acf1e23eba284866a983fe839a7a
Gülçin Baykal <gulcinbaykal@hotmail.com>
2016-12-17 17:16:57 +0300

    added new interface to show current user's current projects

--- templates_operations/dashboard.py
+++ templates_operations/dashboard.py
@@ -4,10 +4,15 @@ from flask import url_for
 from flask import redirect
 from flask import request
 from datetime import datetime
+from flask_login import current_user
 from classes.operations.work_log_operations import work_log_operations
 from classes.operations.project_operations import project_operations
+from classes.operations.project_operations import project_operations
+from classes.operations.person_operations import person_operations
 
 def home_page_config(request):
     store_worklogs = work_log_operations()
+    store_projects = project_operations()
+    active_projects = store_projects.get_the_projects_of_a_person(person_operations.GetPerson(current_user, current_user.email)[0])
     worklogs = store_worklogs.GetFollowedProjectsWorkLogs(2)
-    return render_template('dashboard.html', worklogs=worklogs)
+    return render_template('dashboard.html', worklogs=worklogs, active_projects=active_projects)
